cc03463ef42a27c34f951ac3e6c573a70fa267fd
AmirrezaAsk <raskarpour@gmail.com>
2022-04-24 01:44:10 +0430

    get personal access token from env

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()>{
-    let ghc = github::GithubClient::new(PERSONAL_ACCESS_TOKEN.to_string(), None);
+    let personal_access_token = std::env::var("NOTEPAD_GITHUB_PERSONAL_ACCESS_TOKEN")?;
+    let ghc = github::GithubClient::new(personal_access_token.to_string(), None);
     let gist_id = ghc.create_gist().await?;
     println!("{}", gist_id);
 
