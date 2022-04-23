9f9ccbfa7582db82c8f5aee665006a976403e86e
AmirrezaAsk <raskarpour@gmail.com>
2022-04-24 02:26:43 +0430

    reading values from a config file

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
 mod github;
 mod telegram;
 use anyhow::Result;
+use serde_derive::{Deserialize, Serialize};
+use toml::Value;
+
+#[derive(Deserialize)]
+struct Config {
+    pub personal_access_token: String,
+    pub gist_id: String,
+}
+
+impl Config {
+    async fn new(file_name: &str) -> Result<Self> {
+        let config_file_content = tokio::fs::read_to_string(file_name).await?;
+        let config: Config = toml::from_str(&config_file_content)?;
+        Ok(config)
+    }
+}
 
 
 #[tokio::main]
 async fn main() -> Result<()>{
-    let personal_access_token = std::env::var("NOTEPAD_GITHUB_PERSONAL_ACCESS_TOKEN")?;
-    let ghc = github::GithubClient::new(personal_access_token.to_string(), None);
+    let config = Config::new("config.toml").await?;
+    let ghc = github::GithubClient::new(config.personal_access_token, None);
     let gist_id = ghc.create_gist().await?;
     println!("{}", gist_id);
-
     Ok(())
 }
