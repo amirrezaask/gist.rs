1ee35dcfd7f405fa284a8ad32f80918c21ff2a55
AmirrezaAsk <raskarpour@gmail.com>
2022-04-24 01:41:45 +0430

    now gist creation is ok

--- src/github.rs
+++ src/github.rs
@@ -1,16 +1,32 @@
 use anyhow::Result;
-struct GithubClient {
+pub struct GithubClient {
     personal_access_token: String,
     gist_id: Option<String>,
 }
 
 impl GithubClient {
-   pub async fn create_gist(&self) -> Result<String> {
-        let app = octocrab::OctocrabBuilder::new().personal_token(self.personal_access_token.clone()).build()?;
+    pub fn new(personal_access_token: String, gist_id: Option<String>) -> Self {
+        Self {
+            personal_access_token, gist_id
+        }
+
+    }
+    pub async fn create_gist(&self) -> Result<String> {
+        let app = octocrab::OctocrabBuilder::new()
+            .personal_token(self.personal_access_token.clone())
+            .build()?;
         let gist = match &self.gist_id {
-            Some(gist_id) => app.gists().get(gist_id).await?, 
-            None => app.gists().create().public(false).description("my github gist").file("notes", "").send().await?,
+            Some(gist_id) => app.gists().get(gist_id).await?,
+            None => {
+                app.gists()
+                    .create()
+                    .public(false)
+                    .description("my github gist")
+                    .file("notes.txt", "lala")
+                    .send()
+                    .await?
+            }
         };
         Ok(gist.id)
-   }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 mod github;
 mod telegram;
+use anyhow::Result;
 
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() -> Result<()>{
+    let ghc = github::GithubClient::new(PERSONAL_ACCESS_TOKEN.to_string(), None);
+    let gist_id = ghc.create_gist().await?;
+    println!("{}", gist_id);
+
+    Ok(())
 }
